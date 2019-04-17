75e83556aea8081e052468ec24b169c94bc5a361
vagrant <vagrant@vm3110-2018fa>
2019-04-17 00:17:59 -0400

    fixed terminalPrint

--- a6/terminalPrint.ml
+++ a6/terminalPrint.ml
@@ -1,4 +1,4 @@
-open Objects 
+(* open Objects  *)
 
 let printMap map = 
   let rec printMap_helper (tile: Tile.t) =
@@ -8,7 +8,9 @@ let printMap map =
     | Road -> print_string "\027[0m  ";
     | Civ civ -> let infected = !(civ.infected) in let population = civ.population in
       if infected > 0 then if infected = population then (print_string "\027[31m\027[47m≡≡") else
-        if infected > population/2 then (print_string "\027[31m\027[47m::") in
+        if infected > population/2 then (print_string "\027[31m\027[47m::")
+        else (print_string "\027[31m\027[47m··")
+      else (print_string "\027[47m  ";) in 
 
   let rec printMap_helper2 map count =
     if count = (Array.length map) then  ( print_string "\027[0m\n \027[31mDone." )  
