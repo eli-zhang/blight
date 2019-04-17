201e48c381e9af8f729c353e30afa2bd3d630d19
ekz5 <ekz5@cornell.edu>
2019-04-16 22:19:47 -0400

    zozozo

--- a6/objects.ml
+++ a6/objects.ml
@@ -5,32 +5,31 @@ type disease = {
 }
 
 type civilization = {
-  infected : int ref;
-  population : int;
-  neighbors : (civilization ref * tile ref) list;
+  total_infected : int ref;
+  total_population : int;
+  neighbors : (civilization * tile) list;
 } 
-and tile = Civ of civilization | Land | Water | Road
-
+and tile_type = Land | Water | Road | Civ of civilization
+and tile = {
+  tile_type: tile_type;
+  population: int;
+  infected: int ref;
+}
 (* let rec infect_civilizations (acc: civilization list) 
     (civilizations: civilization list) (disease: disease) =
    match civilizations with
    | [] -> acc
    | (civ::t) -> 
-    let infected = civ.infected in
-    let population = civ.population in
+    let infected = civ.total_infected in
+    let population = civ.total_population in
     let neighbors = 
       if (!infected / population * 100) > disease.civ_to_civ_spread 
       then let rec infect_neighbors neighbors =
              match neighbors with
              | [] -> ();
-             | (civ, tile)::t -> if !(civ.infected) = 0 
-               then civ.infected := 1;
+             | (civ, tile)::t -> if !(civ.total_infected) = 0 
+               then civ.total_infected := 1;
                tile.infected := 1; 
                infect_neighbors t in
-        infect_neighbors C.neighbors;
-        C.neighbors
-
-
-          infect_civilizations (InfectCivilizations (civ) (disease))::acc t disease
-
-   end *)
+        infect_neighbors civ.neighbors in
+        neighbors *)
