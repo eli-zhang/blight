f44ba8cf3885e459defd07ab1a8439895e64d0be
Jason Pan <jp873@cornell.edu>
2019-04-17 20:01:13 -0400

    added docs to randomMap.ml

--- a6/randomMap.ml
+++ a6/randomMap.ml
@@ -8,39 +8,32 @@ let width= 50
 let map = Array.make_matrix length width Land;;
 
 
-let rec placeRiverUp map (x,y) =
-  map.(x).(y) <- Water;
-  map.(x+1).(y) <- Water;
-  if(x<0 || y<0 || x>(length-3) || y>(width-2))  then () else placeRiverUp map (x+1,y+1)
-
-let rec placeRiverDown map (x,y)=
-  map.(x).(y) <- Water;
-  map.(x).(y-1)<- Water;
-  if(x<1 || y<2 ||x>(length-1) ||y>(width-1)) then () else placeRiverDown map (x-1,y-1)
 
 
+(** [getCivilizations map n acc] returns a list with length n that represents civilizations *)
 let rec getCivilizations map n acc =
   let randx = Random.int (length-1) in
   let randy = Random.int (width-1) in 
   if randx= 0 || randx = (length-1) || randy = 0 || randy = (width-1) || map.(randx).(randy) = Civ || map.(randx+1).(randy) = Civ||
      map.(randx-1).(randy) = Civ|| map.(randx).(randy+1) = Civ || map.(randx).(randy-1) = Civ then getCivilizations map n acc else
-    let size = Random.int 20+10 in
+    let size = Random.int 5+5 in
     match n with
     | 0 -> acc
     | _ -> getCivilizations map (n-1) ( ((randx,randy), size):: acc)
 
 
-let rec closestNonCiv map (x,y) = 
-  let dir = Random.int 4 in
-  if (dir =1 && x-1 <> -1 && map.(x-1).(y) <> Civ) then (x-1,y)
-  else if (dir = 2 &&x+1<>length && map.(x+1).(y)<> Civ) then (x+1,y)
-  else if (dir = 3 && y-1 <> -1 && map.(x).(y-1) <> Civ) then (x,y-1)
-  else if (dir = 4 && y+1 <> width && map.(x).(y+1) <> Civ) then (x,y+1)
-  else closestNonCiv map (x,y)
 
 
-
+(** [placeCivilizations map civs] indexes the matrix map with civilization tiles using the
+    information in civs  *)
 let rec placeCivilizations map civs = 
+  let rec closestNonCiv map (x,y) = 
+    let dir = Random.int 4 in
+    if (dir =1 && x-1 <> -1 && map.(x-1).(y) <> Civ) then (x-1,y)
+    else if (dir = 2 &&x+1<>length && map.(x+1).(y)<> Civ) then (x+1,y)
+    else if (dir = 3 && y-1 <> -1 && map.(x).(y-1) <> Civ) then (x,y-1)
+    else if (dir = 4 && y+1 <> width && map.(x).(y+1) <> Civ) then (x,y+1)
+    else closestNonCiv map (x,y) in
   match civs with
   | [] -> ();
   | h::t -> 
@@ -53,7 +46,9 @@ let rec placeCivilizations map civs =
     placeCiv h;
     placeCivilizations map t
 
-
+(** [getBridges map civs staticciv] indexes the matrix map with bridges that
+    connect each civ in civs to the other civ in civs, staticciv is the same list
+    as civs, *)
 let rec getBridges map civs staticciv=
   match civs with
   | [] -> ();
@@ -87,8 +82,18 @@ let rec getBridges map civs staticciv=
 
 
 
-
+(**[generateWaterCiv map] simply indexes the matrix map with the water, bridges, and civ tiles
+   in the correct spots *)
 let generateWaterCiv map = 
+  let rec placeRiverUp map (x,y) =
+    map.(x).(y) <- Water;
+    map.(x+1).(y) <- Water;
+    if(x<0 || y<0 || x>(length-3) || y>(width-2))  then () else placeRiverUp map (x+1,y+1) in
+
+  let rec placeRiverDown map (x,y)=
+    map.(x).(y) <- Water;
+    map.(x).(y-1)<- Water;
+    if(x<1 || y<2 ||x>(length-1) ||y>(width-1)) then () else placeRiverDown map (x-1,y-1) in
   let randx = Random.int (length-1) in
   let randy = Random.int (width-1) in
   let nofciv = (Random.int 30) +10 in
@@ -102,9 +107,7 @@ let generateWaterCiv map =
 
 
 
-
-
-
+(** [printMap map] prints the matrix map *)
 let printMap map = 
   let rec printMap_helper  = function
     | Land -> print_string "\027[40m  "; 
