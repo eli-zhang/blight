922a6777a12461c0e10d4ec20108055447445af0
Jason Pan <jp873@cornell.edu>
2019-04-21 23:46:33 -0400

    fixed bugs

--- a6/randomMap.ml
+++ a6/randomMap.ml
@@ -8,13 +8,12 @@ let map = Array.make_matrix length width Land;;
 (** [getCivilizations map n acc] returns a list with length n that represents 
     civilizations *)
 let rec getCivilizations map n acc =
-  let randx = Random.int (length-1) in
-  let randy = Random.int (width-1) in 
+  let randx = Random.int (length) in
+  let randy = Random.int (width) in 
   if randx= 0 || randx = (length-1) || randy = 0 || randy = (width-1) 
-     || map.(randx).(randy) = Civ || map.(randx+1).(randy) = Civ||
-     map.(randx-1).(randy) = Civ|| map.(randx).(randy+1) = Civ 
-     || map.(randx).(randy-1) = Civ then getCivilizations map n acc else
-    let size = Random.int 5+5 in
+     || map.(randx).(randy) = Civ 
+  then getCivilizations map n acc else
+    let size = Random.int 10+5 in
     match n with
     | 0 -> acc
     | _ -> getCivilizations map (n-1) ( ((randx,randy), size):: acc)
@@ -24,10 +23,10 @@ let rec getCivilizations map n acc =
 let rec placeCivilizations map civs = 
   let rec closestNonCiv map (x,y) = 
     let dir = Random.int 4 in
-    if (dir =1 && x-1 <> -1 && map.(x-1).(y) <> Civ) then (x-1,y)
-    else if (dir = 2 &&x+1<>length && map.(x+1).(y)<> Civ) then (x+1,y)
-    else if (dir = 3 && y-1 <> -1 && map.(x).(y-1) <> Civ) then (x,y-1)
-    else if (dir = 4 && y+1 <> width && map.(x).(y+1) <> Civ) then (x,y+1)
+    if (  dir = 0 && x-1 > -1) then (x-1,y)
+    else if ( dir = 1 &&x+1 < (length) ) then (x+1,y)
+    else if ( dir=2 &&y-1 > -1 ) then (x,y-1)
+    else if ( dir = 3&& y+1 < width ) then (x,y+1)
     else closestNonCiv map (x,y) in
   match civs with
   | [] -> ();
@@ -75,21 +74,20 @@ let generateWaterCiv map =
   let rec placeRiverUp map (x,y) =
     map.(x).(y) <- Water;
     map.(x+1).(y) <- Water;
-    if(x<0 || y<0 || x>(length-3) || y>(width-2))  
-    then () else placeRiverUp map (x+1,y+1) in
+    if(x>(-2) && y>(-2) && x<(length-2) && y<(width-1))  
+    then placeRiverUp map (x+1,y+1) else () in
 
   let rec placeRiverDown map (x,y)=
     map.(x).(y) <- Water;
     map.(x).(y-1)<- Water;
-    if(x<1 || y<2 ||x>(length-1) ||y>(width-1)) 
-    then () else placeRiverDown map (x-1,y-1) in
+    if(x>(0) && y>(1) && x<(length) &&y<(width)) 
+    then placeRiverDown map (x-1,y-1) else () in
   let randx = Random.int (length-1) in
   let randy = Random.int (width-1) in
   (* let nofciv = (Random.int 30) +10 in *)
   placeRiverUp map (randx,randy);
   placeRiverDown map (randx,randy);
-
-  let civilizations = getCivilizations map 5 [] in
+  let civilizations = getCivilizations map 10 [] in
   placeCivilizations map civilizations;
   getBridges map civilizations civilizations
 
