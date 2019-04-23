9600bd2e0c0a5a50bd9b11134a791df00e1729c1
Jason Pan <jp873@cornell.edu>
2019-04-23 19:58:29 -0400

    fixed some things

--- a6/randomMap.ml
+++ a6/randomMap.ml
@@ -1,4 +1,5 @@
 #use "objects.ml"
+open Objects
 
 
 let map = Array.make_matrix 40 50 Tile.Land
@@ -9,25 +10,27 @@ let civs = Array.make 10 (0,0)
 
 (** [getCivilizations map n acc] returns a list with length n that represents 
     civilizations *)
-let rec getCivilizations map n acc s civs=
+let rec getCivilizations (map: Tile.t array array) n acc s civs=
   let length = (Array.length map) in
   let width = (Array.length (Array.get map 0)) in
   let randx = Random.int (length) in
   let randy = Random.int (width) in 
   if randx= 0 || randx = (length-1) || randy = 0 || randy = (width-1) 
-     || (match map.(randx).(randy) with | Tile.Civ _ -> true  | _ -> false)
+     || (match map.(randx).(randy).tile_type with | Civ _ -> true  | _ -> false)
   then getCivilizations map n acc s civs else
     let size = Random.int s+5 in
     match n with
     | 0 -> acc
     | _ -> civs.(n-1) <- (randx,randy); getCivilizations map (n-1) (((randx,randy), size) :: acc ) s civs
 
+
+
 (** [placeCivilizations map civs] indexes the matrix map with civilization 
     tiles using the information in civs  *)
-let rec placeCivilizations map civs = 
+let rec placeCivilizations (map: Tile.t array array) civs = 
   let length = (Array.length map) in
   let width = (Array.length (Array.get map 0)) in
-  let rec closestNonCiv map (x,y) = 
+  let rec closestNonCiv (map: Tile.t array array) (x,y) = 
     let dir = Random.int 4 in
     if (  dir = 0 && x-1 > -1) then (x-1,y)
     else if ( dir = 1 &&x+1 < (length) ) then (x+1,y)
@@ -37,11 +40,15 @@ let rec placeCivilizations map civs =
   match civs with
   | [] -> ();
   | h::t -> 
+    let civilization = Civilization.{infected =ref 0; living =ref 0; dead= ref 0; population=0;neighbors=[] } in
     let rec placeCiv ((x,y),size) =
-      map.(x).(y) <- Tile.Civ{infected = ref 0; population = 50; neighbors = [] };
-      if (size > 0) then
-        let coord = closestNonCiv map (x,y) in placeCiv (coord, size-1)
-      else  ();
+      match (map.(x).(y)).tile_type with 
+      | Civ _ -> map.(x).(y) <- Tile.{tile_type = Civ civilization; infected =0; living = 0; dead=0;population = 50};
+      | _ -> map.(x).(y) <- Tile.{tile_type = (Civ {civilization with population = civilization.population +50});
+                                  infected = 0; living= 50; dead=0; population =50 };
+        if (size > 0) then
+          let coord = closestNonCiv map (x,y) in placeCiv (coord, size-1)
+        else  ();
     in 
     placeCiv h;
     placeCivilizations map t
@@ -49,7 +56,7 @@ let rec placeCivilizations map civs =
 (** [getBridges map civs staticciv] indexes the matrix map with bridges that
     connect each civ in civs to the other civ in civs, staticciv is the same list
     as civs, *)
-let rec getBridges map civs staticciv=
+let rec getBridges (map: Tile.t array array) civs staticciv=
   let length = (Array.length map) in
   let width = (Array.length (Array.get map 0)) in
   match civs with
@@ -61,35 +68,44 @@ let rec getBridges map civs staticciv=
       | (c,s)::t ->
         (let rec makeBridge (x1,y1) (x2,y2) =
            if(x1<x2 && x1<length-1 ) then  
-             (if (map.(x1+1).(y1)= Tile.Civ{infected = ref 0;population = 50;neighbors = []}) then makeBridge ((x1+1),y1) (x2,y2)
-              else map.(x1+1).(y1) <- Tile.Road; makeBridge (x1+1,y1) (x2,y2) )
+
+             match (map.(x1+1).(y1)).tile_type with
+             | Civ _ -> makeBridge ((x1+1),y1) (x2,y2)
+             | _ -> map.(x1+1).(y1) <- Tile.{tile_type = Road; infected = 0; living =0; dead=0; population =0}; makeBridge (x1+1,y1) (x2,y2)
+
            else if (x1>x2 && x1>0) then 
-             (if (map.(x1-1).(y1) = Tile.Civ{infected = ref 0;population = 50;neighbors = []}) then makeBridge ((x1-1),y1) (x2,y2)
-              else map.(x1-1).(y1) <- Tile.Road; makeBridge (x1-1,y1) (x2,y2))
+             match (map.(x1-1).(y1)).tile_type with
+             | Civ _ -> makeBridge ((x1-1),y1) (x2,y2)
+             | _ -> map.(x1-1).(y1) <- Tile.{tile_type = Road; infected = 0; living =0; dead=0; population =0}; makeBridge (x1-1,y1) (x2,y2)
+
            else if (y1<y2 && y1<width-1) then 
-             (if (map.(x1).(y1+1) = Tile.Civ{infected = ref 0;population = 50;neighbors = []}) then makeBridge (x1,(y1+1)) (x2,y2)
-              else map.(x1).(y1+1) <- Tile.Road; makeBridge (x1,(y1+1)) (x2,y2))
+             match (map.(x1).(y1+1)).tile_type with
+             | Civ _ -> makeBridge ((x1),y1+1) (x2,y2)
+             | _ -> map.(x1).(y1+1) <- Tile.{tile_type = Road; infected = 0; living =0; dead=0; population =0}; makeBridge (x1,y1+1) (x2,y2)
+
            else if (y1>y2 && y1>0) then 
-             (if (map.(x1).(y1-1) = Tile.Civ{infected = ref 0;population = 50;neighbors = []}) then makeBridge (x1,(y1-1)) (x2,y2)
-              else map.(x1).(y1-1) <- Tile.Road; makeBridge (x1,(y1-1)) (x2,y2))
+             match (map.(x1).(y1-1)).tile_type with
+             | Civ _ -> makeBridge ((x1),y1-1) (x2,y2)
+             | _ -> map.(x1).(y1-1) <- Tile.{tile_type = Road; infected = 0; living =0; dead=0; population =0}; makeBridge (x1,y1-1) (x2,y2)
+
            else () in
          makeBridge c coords; connectCivs (coords,size) t) in 
     connectCivs h staticciv; getBridges map t staticciv
 
 
 
-let rec placeRivers map n = 
+let rec placeRivers (map: Tile.t array array) n = 
   let length = (Array.length map) in
   let width = (Array.length (Array.get map 0)) in
-  let rec placeRiverUp map (x,y) =
-    map.(x).(y) <-Tile.Water;
-    map.(x+1).(y) <- Water;
+  let rec placeRiverUp (map: Tile.t array array) (x,y) =
+    map.(x).(y) <-Tile.{tile_type= Water; infected= 0; living=0;dead=0;population=0};
+    map.(x+1).(y) <- Tile.{tile_type= Water; infected= 0; living=0;dead=0;population=0};
     if(x>(-2) && y>(-2) && x<(length-2) && y<(width-1))  
     then placeRiverUp map (x+1,y+1) else () in
 
-  let rec placeRiverDown map (x,y)=
-    map.(x).(y) <- Tile.Water;
-    map.(x).(y-1)<-Tile.Water;
+  let rec placeRiverDown (map: Tile.t array array) (x,y)=
+    map.(x).(y) <- Tile.{tile_type= Water; infected= 0; living=0;dead=0;population=0};
+    map.(x).(y-1)<-Tile.{tile_type= Water; infected= 0; living=0;dead=0;population=0};
     if(x>(0) && y>(1) && x<(length) &&y<(width)) 
     then placeRiverDown map (x-1,y-1) else () in
   if n>0 then  
@@ -102,7 +118,7 @@ let rec placeRivers map n =
 
 (**[generateMap map] simply indexes the matrix map with the water, bridges, and civ tiles
    in the correct spots *)
-let generateMap map civs size= 
+let generateMap (map: Tile.t array array) civs size= 
   let nofcivs = Array.length civs in
 
 
@@ -116,15 +132,16 @@ let generateMap map civs size=
 
 
 (** [printMap map] prints the matrix map *)
-let printMap map = 
+let printMap (map: Tile.t array array) = 
 
-  let rec printMap_helper  = function
-    | Tile.Land -> print_string "\027[40m  "; 
-    | Tile.Water -> print_string "\027[46m  "; 
-    | Tile.Road -> print_string "\027[0m  ";
-    | Tile.Civ _ -> print_string "\027[47m  "; in
+  let rec printMap_helper (tile:Tile.t) = 
+    match tile.tile_type with
+    | Land -> print_string "\027[40m  "; 
+    | Water -> print_string "\027[46m  "; 
+    | Road -> print_string "\027[0m  ";
+    | Civ _ -> print_string "\027[47m  "; in
 
-  let rec printMap_helper2 map count =
+  let rec printMap_helper2 (map: Tile.t array array) count =
     if (count = Array.length map) then 
       (print_string "\027[0m\n \027[31mDone." )
     else ( (Array.iter printMap_helper (Array.get map count;));
